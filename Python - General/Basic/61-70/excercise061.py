1cec74eba8fbcf12949c8c24b8c28da27a247173
Piotr Marcol <piotr.marcol@gmail.com>
2017-10-02 14:15:55 +0200

    061

---
+++ Python - General/Basic/61-70/excercise061.py
@@ -0,0 +1,44 @@
+"""
+Write a Python program to convert the distance (in feet) to inches, yards, and miles.
+"""
+
+"""
+SOLUTION:
+"""
+
+from math import sqrt
+import sys
+import os
+from pathlib import Path
+
+abspath = Path(__file__)
+toolspath = abspath.parents[3]
+
+sys.path.append(str(toolspath / 'tools'))
+
+from GetFromUser import GetNonNegativeInteger
+
+feetConversionRatioTable = {
+    "inches" : 12,
+    "yards" : 1/3,
+    "miles" : 1/5280
+}
+
+
+def feetConverter(feet, targetUnit):
+    try:
+        ratio = feetConversionRatioTable[targetUnit]
+    except KeyError:
+        print("Unknown target unit.")
+        return
+    else:
+        return feet*ratio
+
+
+#Get the feet:
+print("Lets set the distance (in feet):")
+feet = GetNonNegativeInteger()
+
+print("%d feet is %s inches." % (feet, feetConverter(feet,"inches")))
+print("%d feet is %s yards." % (feet, feetConverter(feet,"yards")))
+print("%d feet is %s miles." % (feet, feetConverter(feet,"miles")))
