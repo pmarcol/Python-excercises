644ef1225457084d9ba05bd90a83ea3f4effb7b5
Piotr Marcol <piotr.marcol@gmail.com>
2017-10-04 15:45:22 +0200

    062

---
+++ Python - General/Basic/61-70/excercise062.py
@@ -0,0 +1,49 @@
+"""
+Write a Python program to get an absolute file path.
+"""
+
+"""
+SOLUTION:
+"""
+
+import sys
+import os
+from pathlib import Path
+
+abspath = Path(__file__)
+toolspath = abspath.parents[3]
+
+sys.path.append(str(toolspath / 'tools'))
+
+from GetFromUser import GetNonNegativeInteger
+
+def convertToSeconds(value, originalUnit):
+    try:
+        ratio = toSecondsConvertionRatioTable[originalUnit]
+    except KeyError:
+        print("Unknown unit: %s" % originalUnit)
+        return
+    else:
+        return value*ratio
+
+toSecondsConvertionRatioTable = {
+    "seconds" : 1,
+    "minutes" : 60,
+    "hours" : 60*60,
+    "days" : 24*60*60
+}
+
+convertionValuesTable = {}
+
+for myKey in toSecondsConvertionRatioTable.keys():
+    print("%s:" % myKey)
+    myValue = GetNonNegativeInteger()
+    convertionValuesTable[myKey] = myValue
+
+output = 0
+
+for myKey in convertionValuesTable:
+    output += convertToSeconds(convertionValuesTable[myKey], myKey)
+    print("%d %s," % (myValue, myKey), end = " ")
+
+print("give %d seconds in total" % output)
